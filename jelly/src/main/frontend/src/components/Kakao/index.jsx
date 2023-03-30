677a67f09f84803e296a720ecf403b122b8bed46
이태형 <spdhsrnvl456@gmail.com>
2023-03-30 18:59:30 +0900

    0330 인가코드 수정

--- jelly/src/main/frontend/src/components/Kakao/index.jsx
+++ jelly/src/main/frontend/src/components/Kakao/index.jsx
@@ -7,19 +7,20 @@ export default function KakaoLogin(){
 
     useEffect(()=>{
         try{
+            
             const url = new URL(window.location.href);
-            const code = url.searchParams.get("code");
-            console.log(code);
+            let code = url.herf
+            // const code1 = url.searchParams.get("code");
+            // console.log(code1)
             if(code){
                 axios({
                     url : "/oauth/login/kakao", //설정된 서버 url
                     method : "post",
-                    data : {
-                        code : code,
-                    }
+                    data : code
                 }).then((result)=>{
-                    const accessToken = result.data.split("=")[1].split("&")[0];
-                    console.log(accessToken) //Token 값 출력
+                    // const accessToken = result.data.split("=")[1].split("&")[0];
+                    // console.log(accessToken) //Token 값 출력
+                    console.log(result)
                 })
             }
         }catch(error){
