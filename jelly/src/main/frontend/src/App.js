d7db705184243b02b4256aea0194060e026d2917
이태형 <spdhsrnvl456@gmail.com>
2023-04-01 21:43:14 +0900

    0401 taeheyong style:로그인버튼 스타일링

--- jelly/src/main/frontend/src/App.js
+++ jelly/src/main/frontend/src/App.js
@@ -1,22 +1,13 @@
 // src/main/frontend/src/App.js
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import axios from 'axios';
 import { BrowserRouter,Routes, Route } from "react-router-dom"
 import Home from './pages/Home';
 import KakaoLogin from './components/Kakao';
 
 function App() {
-//    const [hello, setHello] = useState('')
-
-//     useEffect(() => {
-//         axios.get('/hello')
-//         .then(response => setHello(response.data))
-//         .catch(error => console.log(error))
-//     }, []);
-
     return (
         <div className="App">
-            {/* 백엔드에서 가져온 데이터입니다 : {hello} */}
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
