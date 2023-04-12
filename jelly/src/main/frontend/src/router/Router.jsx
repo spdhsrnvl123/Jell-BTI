f068bd3aa7d0a7f48af4eeecda7d7e4a5c8503be
ehaakdl-diary <jaeho13@naver.com>
2023-04-12 13:09:50 +0900

    0412 jaeho board

--- jelly/src/main/frontend/src/router/Router.jsx
+++ jelly/src/main/frontend/src/router/Router.jsx
@@ -16,8 +16,11 @@ const Router = () => {
                 <Route path="/login" element={<KakaoLogin />} />
                 <Route path="/detail" element={<DetailPage />} />
                 <Route path="/mypage" element={<MyPage />} />
+
                 <Route path="/home" element={<Home />} />
                 <Route path="/board" element={<Board />} />
+
+
             </Routes>
         </BrowserRouter>
     )
