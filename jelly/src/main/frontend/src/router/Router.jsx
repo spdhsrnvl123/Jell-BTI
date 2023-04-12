f12364371990b3681bb1d4daa4590a3514fc4a71
ehaakdl-diary <jaeho13@naver.com>
2023-04-12 13:06:03 +0900

    0412 jaeho board

--- jelly/src/main/frontend/src/router/Router.jsx
+++ jelly/src/main/frontend/src/router/Router.jsx
@@ -5,9 +5,10 @@ import MyPage from "../pages/MyPage"
 import KakaoRedirect from "../pages/KakaoRedirect"
 import KakaoLogin from "../pages/KakaoLoginPage"
 import DetailPage from "../pages/DetailPage"
+import Board from "../pages/Board/Board"
 
-const Router = ()=>{
-    return(
+const Router = () => {
+    return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<StartPage />} />
@@ -16,6 +17,7 @@ const Router = ()=>{
                 <Route path="/detail" element={<DetailPage />} />
                 <Route path="/mypage" element={<MyPage />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/board" element={<Board />} />
             </Routes>
         </BrowserRouter>
     )
