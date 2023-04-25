50e1dc81655c1d4e8bc25b851fd3b33f79dc295e
이태형 <spdhsrnvl456@gmail.com>
2023-04-25 22:36:22 +0900

    0425 taehyeong ✨feat : Auth hook 구현

---
+++ jelly/src/main/frontend/src/hooks/useAuth.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+
+const useAuth = () => {
+  const token = localStorage.getItem('token');
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (!token) {
+      navigate('/login');
+    }else if(token){
+      navigate('/mypage')
+    }
+  }, [token, navigate]);
+
+  return token;
+};
+
+export default useAuth;
