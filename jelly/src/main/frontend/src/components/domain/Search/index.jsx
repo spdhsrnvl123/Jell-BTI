fffa99a1e0cf94b70448b885e3aee1282d18bebe
이태형 <spdhsrnvl456@gmail.com>
2023-04-25 22:37:36 +0900

    0425 taehyeong ♻︎refactor : Input hook구현으로 수정

--- jelly/src/main/frontend/src/components/domain/Search/index.jsx
+++ jelly/src/main/frontend/src/components/domain/Search/index.jsx
@@ -1,103 +1,52 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import SearchBox from "../SearchBox"
-import {useNavigate} from "react-router-dom"
 import axios from "axios";
-import {useDispatch, useSelector} from "react-redux"
-import { jellyInfoGet } from "../../../redux/store";
+import { useInput } from "hooks/useInput";
 
 const Container = styled.div`
     display: flex;
-    margin: 0 auto;
-    align-items: center;    
+    justify-content: center;
 `
 
 const InputStyle = styled.input`
-    position: absolute;
-    top:34%;
-    transform: translate(-60%,0);
-    left: 50%;
+    margin: 0 auto;
     background: #F5F5F5;
-    width:23.5em;
-    border: 5px solid #FFFFFF;
-    border-radius: 40px;
-    font-style: normal;
+    border: 3px solid #FFFFFF;
+    border-top-left-radius: 40px;
+    border-bottom-left-radius: 40px;
     font-weight: 700;
-    font-size: 18px;
-    padding-left: 2%;
-    padding-top: 1%;
-    padding-bottom: 1%;
+    width: 412px;
+    font-size: 20px;
+    padding: 12px 40px;
+    z-index: 99;
 `
 const Button = styled.button`
-    position: absolute;
-    top:34.7%;
-    transform: translate(132%,0);
-    left: 50%;
     background: rgba(0, 163, 255, 0.2);
-    border-radius: 0px 40px 40px 0px;
-    z-index: -1;
-    width: 7em;
-    height: 2.5em;
-    gap: 10px;
-    font-size: 18px;
+    border-top-right-radius: 40px;
+    border-bottom-right-radius: 40px;
+    z-index: 1;
+    font-size: 20px;
+    padding : 15px 16px;
     border: 0;
-    color: #00A3FF;
     cursor: pointer;
 `
 
 const Search = ()=>{
-    const [value, setValue] = useState("");
-    const [boolean, setBoolean] = useState(true)
-    const navigate = useNavigate()
-
-    const onChange = (e)=>{
-        setValue(e.target.value);
-        setBoolean(false);
-    }
-    
-    useEffect(()=>{
-        if(value === ""){
-            setBoolean(true)
-        }
-    },[value])
-
-    //젤리 검색 엔터 눌르면 해당 라우터로 이동
-    const search = (event) =>{
-        event.preventDefault()
-        console.log(event.target[0].value)
-            let keyword = event.target[0].value
-            navigate(`/home/?jellyName=${keyword}`)
-    }
-    
-    const [jelly,setJelly] = useState([])
-    //젤리정보 받아오는 API
-    useEffect(()=>{
-        axios.get("/jellies").then((response)=>{
-            // console.log(response.data.slice(0,12))
-            let data = setJelly(response.data.slice(0,12))
-            return data;
-        })
-    },[])
-
-    console.log(jelly);
+    const [inputValue, handleChange, boolean, handleSubmit] = useInput('');
 
     return(
-        <>
-        <Container>
-            <form onSubmit={search}>
-                <InputStyle 
-                    value={value} 
-                    // onKeyPress={(event)=>search(event)} 
-                    onChange={onChange} 
-                    placeholder="Search for the jelly you want..." 
-                />
-                <Button>
-                    Search
-                </Button>
-            </form>
-        </Container>
-        <SearchBox boolean={boolean} jelly={jelly} />
-        </>
+            <Container>
+                <form onSubmit={handleSubmit}>
+                    <InputStyle 
+                        value={inputValue} 
+                        onChange={handleChange} 
+                        placeholder="Search for the jelly you want..." 
+                    />
+                    <Button type="submit">
+                        Search
+                    </Button>
+                </form>
+            </Container>
     )
 }
 
