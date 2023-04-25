adb2cf8f80c6e41d856433f7f56a329e9d7b5197
이태형 <spdhsrnvl456@gmail.com>
2023-04-25 22:31:28 +0900

    0425 taehyeong ✨feat : craco설정

--- jelly/src/main/frontend/src/pages/Home/index.jsx
+++ jelly/src/main/frontend/src/pages/Home/index.jsx
@@ -1,15 +1,36 @@
-import Header from "../../components/domain/Header";
-import Navigation from "../../components/domain/Navigation";
-import LoginButton from "../../components/base/Login";
-import Search from "../../components/domain/Search";
+import Header from "@/components/domain/Header";
+import Navigation from "@/components/domain/Navigation";
+import LoginButton from "@/components/base/Login";
+import Search from "@/components/domain/Search";
+import { useDispatch, useSelector } from "react-redux";
+import SearchBox from "components/domain/SearchBox";
+import styled from "styled-components";
+
+const Container = styled.div`
+    height: 43vh;
+    /* background-color: red; */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
 
 const Home = ()=>{
+    const dispatch = useDispatch();
+    const count = useSelector(state=>{
+      return state;
+    });
+
+    console.log(count)
     return(
         <>
             <LoginButton />
-            <Header />
-            <Navigation />
-            <Search />
+            <Container>
+                <Header />
+                <Navigation />
+                <Search />           
+            </Container>
+            <SearchBox />
         </>
     )
 }
