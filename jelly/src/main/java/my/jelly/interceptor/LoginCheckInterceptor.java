2082209cf23dcc827a3db79d28358a85e9efc9fd
qkrmekem <magicofclown@naver.com>
2023-05-26 23:49:54 +0900

    0526 euimin 로그인인터셉터 기능

---
+++ jelly/src/main/java/my/jelly/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,34 @@
+package my.jelly.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+
+        log.info("로그인 인터셉터 실행, requestURI ={}", requestURI);
+
+        HttpSession session = request.getSession();
+
+        log.info("method ={}", request.getMethod());
+
+        if (!request.getMethod().equals("GET")) {
+            if (session == null || session.getAttribute("loginMember") == null) {
+                log.info("비로그인 사용자");
+                response.sendRedirect("/naverTest");
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
