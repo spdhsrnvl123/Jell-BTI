c27499e041097dbd2f66e31ec5d937428ad869b9
이태형 <spdhsrnvl456@gmail.com>
2023-06-14 19:21:28 +0900

    0614 taehyeong 🎨style:로그인모달창 스타일

--- jelly/src/main/frontend/src/components/base/Button/index.jsx
+++ jelly/src/main/frontend/src/components/base/Button/index.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types'
 const Root = styled.button`
     padding: ${({padding})=>padding};
     margin : ${({margin})=>margin};
-    /* background: ${({bgColor})=>bgColor}; */
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    background: ${({bgColor})=>bgColor};
+    /* box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); */
     border-radius: 30px;
     border: 0;
     width: ${({width})=> width};
