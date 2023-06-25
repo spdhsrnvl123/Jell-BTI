1131da8464f3d3868f24a950817c305ea9c99e0a
이태형 <spdhsrnvl456@gmail.com>
2023-06-25 22:27:14 +0900

    0625 taehyeong ✨feat:후기작성 라우터 설정 및 상품상세 전용 모달 구현

---
+++ jelly/src/main/frontend/src/components/domain/GModal/index.jsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Outlet, useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
+import Img from "../../base/Img";
+import Button from "../../base/Button";
+import axios from "axios";
+import Goods from "../Goods";
+
+const ModalCotainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 999;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
+
+const ModalSection = styled.section`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 68vw;
+  height: 50vw;
+  background: #f7fef7;
+  border-radius: 50px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+const ModalButton = styled.button`
+  position: absolute;
+  top: -5%;
+  right: 5%;
+  width: 30px;
+  font-size: 101px;
+  font-weight: 300;
+  border: 0;
+  cursor: pointer;
+  background-color: transparent;
+`;
+
+const ImgBox = styled.div`
+  position: absolute;
+  top: 14%;
+  left: 2.5%;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow-y: auto;
+  margin-left: 37%;
+`;
+
+const Title = styled.div`
+  font-size: 6.5vw;
+  margin-top: 60px;
+  text-shadow: 0px 3px 3px rgba(0, 0, 0, 0.15);
+  margin: 30px auto;
+  width: 340px;
+  text-align: center;
+`;
+
+const GModal = () => {
+  const navigate = useNavigate();
+  const data = useSelector((state) => state.jellyInfo);
+  const { id } = useParams();
+  const [value, setValue] = useState([]);
+  const [score, setScore] = useState(0);
+  let count = parseInt(score);
+
+  useEffect(() => {
+    if (data.status === "complete") {
+      let item = data.value.filter((v) => {
+        return Number(id) === v.jidx;
+      });
+      setValue(...item);
+    }
+  }, [data]);
+
+
+  return (
+    <ModalCotainer>
+      <ModalSection>
+        <Content>
+          <ImgBox>
+            <Img src={value.imageUrl} width={"26vw"} />
+          </ImgBox>
+          <Title>{value.jname}</Title>
+          <Outlet />
+        </Content>
+        <ModalButton onClick={() => navigate("/home")}>&times;</ModalButton>
+      </ModalSection>
+    </ModalCotainer>
+  );
+};
+
+export default GModal;
---
+++ jelly/src/main/frontend/src/components/domain/Review/index.jsx
@@ -0,0 +1,112 @@
+import styled from "styled-components";
+import Button from "../../base/Button";
+import { useInput } from "hooks/useInput";
+import { useParams } from "react-router-dom";
+import StarRating from "../StarRating";
+import { useState } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import useAxios from "hooks/useAxios";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TextArea = styled.textarea`
+  width: 370px;
+  height: 270px;
+  background: #d9d9d9;
+  border-radius: 30px;
+  border: 0;
+  margin-bottom: 20px;
+  font-size: 35px;
+  padding: 0px 20px;
+  outline: none;
+  margin-top: 20px;
+`;
+
+const Wrap = styled.div`
+    display : flex;
+    flex-direction: column;
+    padding-top:15px;
+`
+
+const RatingText = styled.div`
+    color : #787878;
+    font-size : 12px;
+    font-weight : 400;
+`
+
+const Stars = styled.div`
+    display: flex;
+    padding-top:5px;
+    & svg{
+        color:gray;
+        cursor: pointer;
+    }
+    :hover svg {
+    color: #fcc419;
+  }
+  & svg:hover ~ svg {
+    color: gray;
+  }
+  .yellowStar {
+    color: #fcc419;
+  }
+`
+
+const Review = () => {
+  const [inputValue, handleChange] = useInput("");
+  const [count, setCount] = useState(0);
+  const {id} = useParams();
+  const userData = useSelector((state)=>state.userInformation);
+
+  const ReviewHandler = (e)=>{
+    e.preventDefault();
+      axios({
+        url: '/jellies/rates',
+        method: 'post',
+        data: {
+          "jIdx": id,
+          "mNick": userData.mNick,
+          "mJelly": userData.mJelly,
+          "jStar": count,
+          "rContent": inputValue,
+          "mEmail": userData.mEmail
+        }
+    }).then((response) => {
+      console.log(response)
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  return (
+    <>
+      <Form onSubmit={ReviewHandler}>
+        <h1 style={{fontSize:"40px",marginBottom:"20px", marginTop:"-40px"}}>별점을 입력해주세요!</h1>
+        <StarRating setCount={setCount} /> 
+        <TextArea
+         type="text"
+         placeholder="후기를 입력해주세요."
+         value={inputValue}
+         onChange={handleChange}
+        >
+        </TextArea>
+        <Button
+          fontSize={40}
+          bgColor={"#16f916"}
+          fontWeight={600}
+          padding={"1px 14px"}
+          margin={"0px 10px"}
+        >
+          제출하기
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default Review;
