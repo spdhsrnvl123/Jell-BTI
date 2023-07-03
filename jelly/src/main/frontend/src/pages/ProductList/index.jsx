aa610dc0151a9acedbf52ea76fbdb2ac98c5cfc7
이태형 <spdhsrnvl456@gmail.com>
2023-07-03 23:40:54 +0900

    0703 taehyeong 🐛fix:제품리스트 새로고침 오류 수정

--- jelly/src/main/frontend/src/pages/ProductList/index.jsx
+++ jelly/src/main/frontend/src/pages/ProductList/index.jsx
@@ -1,8 +1,10 @@
 import Button from "components/base/Button";
 import Img from "components/base/Img";
 import Modal from "components/base/Modal";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { asyncUpFetch } from "redux/jellyInfo";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -15,11 +17,13 @@ const Container = styled.div`
 const Title = styled.h1`
     font-size : 9vw;
     text-align: center;
+    text-shadow: 0px 3px 3px rgba(0, 0, 0, 0.15);
 `
 
 const SubTitle = styled(Title)`
     font-size :5vw;
     margin-top: 35px;
+    text-shadow: 0px 3px 3px rgba(0, 0, 0, 0.15);
 `
 
 const Name = styled.h2`
@@ -28,23 +32,28 @@ text-align: center;
 `
 
 const ProductList = ()=>{
-    const data = useSelector((state)=>state.jellyInfo);
-    console.log(data.value);
     const navigate = useNavigate()
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(asyncUpFetch());
+    }, []);
+
+    const data = useSelector((state)=> state)
 
     return(
         <Modal>
             <Title>후기 작성하기</Title>
             <Container>
                 {
-                    data.value.map((v)=>{
+                    data.jellyInfo.status === "complete" ? data.jellyInfo.value.map((v)=>{
                         return (
-                            <Button bgColor={"transparent"} onClick={()=>navigate(`/home/goodsModal/${v.jidx}/review`)}>
+                            <Button key={v.jidx} bgColor={"transparent"} onClick={()=>navigate(`/home/goodsModal/${v.jidx}/review`)}>
                                 <Img key={v.jidx} src={v.imageUrl} width={"10vw"} padding={5} cursor={"pointer"} />
                                 <Name>{v.jname.slice(4)}</Name>
                             </Button>
                         )
-                    })
+                    }) : "로딩중입니다..."
                 }
             </Container>
             <SubTitle>클릭해주세요~!</SubTitle>
