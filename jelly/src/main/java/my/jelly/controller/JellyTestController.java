f0d998e554bddcb2fbd9e14285aa06aa39567837
kimyuri <dsuis1310009@naver.com>
2023-07-16 21:06:01 +0900

    jellyTest Controller 수정

--- jelly/src/main/java/my/jelly/controller/JellyTestController.java
+++ jelly/src/main/java/my/jelly/controller/JellyTestController.java
@@ -1,6 +1,5 @@
 package my.jelly.controller;
 
-import jdk.jfr.Frequency;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import my.jelly.entity.Member;
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.text.ParseException;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -27,9 +26,6 @@ public class JellyTestController {
     @Autowired
     JellyTestService jellyTestService;
 
-    @Autowired
-    MemberRepository memberRepository;
-
 
     // 젤리테스트결과 받아오는 컨트롤러
 
@@ -42,16 +38,29 @@ public class JellyTestController {
         return bool;
     }
 
+    // 2. 테스트 결과 젤리 타입 받아서 회원에 저장
     @PostMapping("/jResult")
-    public Map<String,String> userJellyResult(@RequestParam String mJelly, HttpServletRequest request) throws ParseException {
+    public void callJellyResult(@RequestParam String mJelly, HttpServletRequest request) throws ParseException {
         System.out.println("프론트에서 젤리 결과 가져오기 : " + mJelly);
         HttpSession session = request.getSession(false);
         Member member = (Member) session.getAttribute("userInfo");
 
         System.out.println("유저 정보 꺼내오기 성공 "+ member);
 
-        Map<String,String> result = jellyTestService.callBackJellyResult(mJelly, member);
-        return result;
+        jellyTestService.callJellyResult(mJelly, member);
+    }
+
+    // 3. 세션에서 회원 정보를 꺼내와 해당 젤리 테스트 결과를 내려줌
+    @GetMapping("/jResult")
+    public Map<String, String> returnJellyResult(HttpServletRequest request) throws ParseException{
+        Map <String, String> map = new HashMap<>();
+
+        HttpSession session = request.getSession(false);
+        Member member = (Member) session.getAttribute("userInfo");
+
+        map = jellyTestService.backJellyResult(member);
+
+        return map;
     }
 
 }
